ce38fc8098688af8636d3976f000ad974f6da631
rmayungon2 <ayungon.rhea@gmail.com>
2016-12-06 10:37:29 +0800

    removed "your input"

--- Exercise5/Form.php
+++ Exercise5/Form.php
@@ -184,19 +184,6 @@
 			  <br><br>
 			  <input type="submit" name="submit" value="Submit">
 			</form>
-
-			<?php
-			echo "<h2>Your Input:</h2>";
-			echo $name;
-			echo "<br>";
-			echo $email;
-			echo "<br>";
-			echo $website;
-			echo "<br>";
-			echo $comment;
-			echo "<br>";
-			echo $gender;
-			?>
 	</div>
 	</div>
 	<footer>
