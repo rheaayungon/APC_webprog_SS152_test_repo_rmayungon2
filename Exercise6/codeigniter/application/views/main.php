fe720ba7f6922ea13744e261d5dbbc56efde7ac0
Rhea Ayungon <ayungon.rhea@gmail.com>
2016-12-28 21:11:46 +0800

    main.php

---
+++ Exercise6/codeigniter/application/views/main.php
@@ -0,0 +1,56 @@
+<html>
+	<title>
+		MIRIFICAL
+	</title>
+		<style>
+			body {
+				background-image: url("<?php echo base_url();?>img/bg.jpg");
+				background-repeat: no-repeat;
+				background-attachment: fixed;
+			}
+			a:link {
+				color:black;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+				font-weight:bold;
+			}
+			a:visited {
+				color:black;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+			}
+			a:hover {
+				color:white;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+			}
+			a:active {
+				color:black;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+			}
+			div.transbox {
+				margin: 200px;
+				background-color: #ffffff;
+				border: 1px solid black;
+				opacity: 0.6;
+				filter: alpha(opacity=60);
+				padding-bottom: 0.5;
+			}
+			div.transbox p {
+				margin: 5%;
+				font-weight: bold;
+				color: #000000;
+			}
+			p {
+				padding-top: 50px;
+			}
+		</style>
