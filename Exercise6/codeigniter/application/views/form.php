f3d49e8b47cb09ae0bc70b59f47cdf4b0ecacfbd
Rhea Ayungon <ayungon.rhea@gmail.com>
2016-12-28 21:01:09 +0800

    form

---
+++ Exercise6/codeigniter/application/views/form.php
@@ -0,0 +1,80 @@
+<html>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <title>
+		  MIRIFICAL
+  	</title>
+  <style>
+     body{
+      background-image: url("<?php echo base_url();?>img/bg.jpg");
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+    }
+    a:link {
+      color:black;
+      background-color:transparent;
+      text-decoration: none;
+      font-family:courier;
+      font-size:20px;
+      font-weight:bold;
+    }
+    a:visited {
+      color:black;
+      background-color:transparent;
+      text-decoration: none;
+      font-family:courier;
+      font-size:20px;
+    }
+    a:hover {
+      color:white;
+      background-color:transparent;
+      text-decoration: none;
+      font-family:courier;
+      font-size:20px;
+    }
+    a:active {
+      color:black;
+      background-color:transparent;
+      text-decoration: none;
+      font-family:courier;
+      font-size:20px;
+    }
+    h1{
+      text-align:center; 
+      color:black;
+    }
+    table {
+      font-family: quicksand;
+      width: 100%;
+      font-size: 15;
+      padding-left: 50;
+      padding-right: 50;
+    }
+    th{
+      background:#FFD1DC;
+    }
+    td, th {
+      border: 3px solid #000000;
+      text-align: left;
+      padding: 5px;
+    }
+    td{
+      background-color: rgba(255,255,255,.5);
+    }
+    div.transbox {
+      margin: 200px;
+      background-color: #ffffff;
+      border: 1px solid black;
+      opacity: 0.6;
+      filter: alpha(opacity=60);
+      padding-bottom: 0.5;
+    }
+    div.transbox p {
+      margin: 5%;
+      font-weight: bold;
+      color: #000000;
+    }
+    p {
+      padding-top: 50px;
+    }
+    .error {color: #FF0000;}
+    </style>
