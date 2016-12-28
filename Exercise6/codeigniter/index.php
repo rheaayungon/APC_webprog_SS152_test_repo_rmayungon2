8a443910ef49e4cd89cc6079beaae4f45ec467b1
Rhea Ayungon <ayungon.rhea@gmail.com>
2016-12-28 20:44:16 +0800

    started new

--- Exercise6/codeigniter/index.php
+++ Exercise6/codeigniter/index.php
@@ -88,6 +88,7 @@ switch (ENVIRONMENT)
 		echo 'The application environment is not set correctly.';
 		exit(1); // EXIT_ERROR
 }
+
 /*
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
