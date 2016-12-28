e318f6fea632877311a14037174c6ee83b6faa92
Rhea Ayungon <ayungon.rhea@gmail.com>
2016-12-28 20:56:15 +0800

    edit

---
+++ Exercise6/codeigniter/application/views/edit.php
@@ -0,0 +1,67 @@
+<html>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		MIRIFICAL
+	</title>
+	<style>
+		body{
+			background-image: url("<?php echo base_url();?>img/bg.jpg");
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+		}
+		a:link {
+			color:black;
+			background-color:transparent;
+			text-decoration: none;
+			font-family:courier;
+			font-size:20px;
+			font-weight:bold;
+		}
+		a:visited {
+			color:black;
+			background-color:transparent;
+			text-decoration: none;
+			font-family:courier;
+			font-size:20px;
+		}
+		a:hover {
+			color:white;
+			background-color:transparent;
+			text-decoration: none;
+			font-family:courier;
+			font-size:20px;
+		}
+		a:active {
+			color:black;
+			background-color:transparent;
+			text-decoration: none;
+			font-family:courier;
+			font-size:20px;
+		}
+		h1{
+			text-align:center; 
+			color:black;
+		}
+		div.transbox {
+			margin: 200px;
+			background-color: #ffffff;
+			border: 1px solid black;
+			opacity: 0.6;
+			filter: alpha(opacity=60);
+			padding-bottom: 0.5;
+		}
+		div.transbox p {
+			margin: 5%;
+			font-weight: bold;
+			color: #000000;
+		}
+		p {
+			padding-top: 20px;
+		}
+		p{
+			text-align: center;
+			font-size: 18;
+			font-family: courier;
+		}
+		.error {color: #FF0000;}
+	</style>
