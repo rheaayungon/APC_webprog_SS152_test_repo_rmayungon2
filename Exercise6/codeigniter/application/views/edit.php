7094b508067ce24e5a6e504cedb862f9bc4e9343
Rhea Ayungon <ayungon.rhea@gmail.com>
2016-12-28 20:58:42 +0800

    footer and ending

--- Exercise6/codeigniter/application/views/edit.php
+++ Exercise6/codeigniter/application/views/edit.php
@@ -146,3 +146,18 @@
 			</center>
 </form>
 		</div>
+
+		<footer>
+		<center>
+		<hr width="70%">
+		<hr width="70%">
+			<b><p style="color:black;margin-top:7em;margin-bottom:0.1em;font-size:12px;font-family:courier">copyrights 2016</b></p>
+			<center><p style="margin-top:0.1em;margin-bottom:0.1em">
+				<a href="https://www.facebook.com/rheaayungon/"target="_blank"><img a src="<?php echo base_url();?>img/fblogo.png" style="width:50px;height:50px"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://www.instagram.com/rheaayungon/?hl=en"target="_blank"><img src="<?php echo base_url();?>img/iglogo.png" style="width:50px;height:50px;">&nbsp;
+				<a href="https://www.twitter.com/rheaayungon/"target="_blank"><img src="<?php echo base_url();?>img/twitterlogo.png" style="width:52px;height:52px;">
+				</a></center>
+		</center>
+	</footer>
+	</body>
+</html>
