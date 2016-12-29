1adf25aa7e9d44d99464a5d5d793c0af195babf7
Rhea Ayungon <ayungon.rhea@gmail.com>
2016-12-29 12:02:15 +0800

    trivia.php

---
+++ Exercise6/codeigniter/application/views/trivia.php
@@ -0,0 +1,70 @@
+<html>
+	<title>
+		MIRIFICAL
+	</title>
+		<style>
+			body {
+				background-image: url("<?php echo base_url();?>img/bg.jpg");
+				background-repeat: no-repeat;
+				background-attachment: fixed;
+			}
+			a:link {
+				color:black;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+				font-weight:bold;
+			}
+			a:visited {
+				color:black;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+			}
+			a:hover {
+				color:white;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+			}
+			a:active {
+				color:black;
+				background-color:transparent;
+				text-decoration: none;
+				font-family:courier;
+				font-size:20px;
+			}
+			h1{
+			text-align:center; 
+			color:black;
+			}
+			p {
+				font-family:courier;
+				font-size:18px;
+				color:black;
+				text-align:center;
+			}
+			button button 1{
+				font-family:courier;
+				font-size:12px;
+				color:black;
+				text-align:center;
+			}
+			div.transbox {
+				margin: 200px;
+				background-color: #ffffff;
+				border: 1px solid black;
+				opacity: 0.6;
+				filter: alpha(opacity=60);
+			}
+			div.transbox p {
+				font-weight: bold;
+				color: #000000;
+			}
+			p {
+				padding-top: 10px;
+			}
+		</style>
